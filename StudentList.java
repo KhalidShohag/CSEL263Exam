d6a8b72ba5c98ecd442febd5ecc7a1b271968d34
Khalid Shohag <shohag9438@yahoo.com>
2021-12-03 22:54:09 +0600

    Update code for better consistency

--- StudentList.java
+++ StudentList.java
@@ -4,58 +4,56 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
-//		Check arguments
+		//Check arguments
 		if(args[0].equals("a")) {
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+			BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
-			} catch (Exception e){} 
+			for(String j : i) { 
+				System.out.println(j);
+			}
+			} catch (Exception e){
+			
+			} 
 			System.out.println("Data Loaded.");
 		}
-		else if(args[0].equals("r")) 
-		{
+		else if(args[0].equals("r")) {
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+			BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			System.out.println(r);
 			String i[] = r.split(",");	
 			Random x = new Random();
-				int y = x.nextInt();
-					System.out.println(i[y]);
-			} catch (Exception e){} 
+			int y = x.nextInt();
+			System.out.println(i[y]);
+			} catch (Exception e){
+			
+			} 
 			System.out.println("Data Loaded.");			
 		}
 		else if(args[0].contains("+")){
 			System.out.println("Loading data ...");			
 			try {
-			BufferedWriter s = new BufferedWriter(
-					new FileWriter("students.txt", true));
+			BufferedWriter s = new BufferedWriter(new FileWriter("students.txt", true));
 			String t = args[0].substring(1);
-	        Date d = new Date();
-	        String df = "dd/mm/yyyy-hh:mm:ss a";
-	        DateFormat dateFormat = new SimpleDateFormat(df);
-	        String fd= dateFormat.format(d);
+	        	Date d = new Date();
+	        	String df = "dd/mm/yyyy-hh:mm:ss a";
+	        	DateFormat dateFormat = new SimpleDateFormat(df);
+	        	String fd= dateFormat.format(d);
 			s.write(", "+t+"\nList last updated on "+fd);
 			s.close();
-			} catch (Exception e){}
-							
+			} catch (Exception e){
+			}					
 			System.out.println("Data Loaded.");	
 		}
 		else if(args[0].contains("?")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+			BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
 			String r = s.readLine();
 			String i[] = r.split(",");	
 			boolean done = false;
@@ -63,19 +61,18 @@ public class StudentList {
 			for(int idx = 0; idx<i.length && !done; idx++) {
 				if(i[idx].equals(t)) {
 					System.out.println("We found it!");
-						done=true;
+					done=true;
 				}
 			}
-			} catch (Exception e){} 
+			} catch (Exception e){
+			} 
 			System.out.println("Data Loaded.");				
 		}
 		else if(args[0].contains("c")) 
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
+			BufferedReader s = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt"))); 
 			String D = s.readLine();
 			char a[] = D.toCharArray();			
 			boolean in_word = false;
@@ -83,12 +80,18 @@ public class StudentList {
 			for(char c:a) {
 				if(c ==' ') 
 				{
-					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+					if (!in_word) {
+					    count++; 
+					    in_word =true;
+					}
+					else {
+					    in_word=false;
+					}			
 				}
 			}
 			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
+			} catch (Exception e){
+			} 
 			System.out.println("Data Loaded.");				
 		}
 	}
